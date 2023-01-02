0df199fce8ca20f4c5f538f66ff4fee8d5f94deb
Mia <tjwlfif@gmail.com>
2023-01-02 17:55:33 +0900

    Modify: 초기세팅 최종 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import './styles/common.scss';
 import './styles/reset.scss';
-import './styles/variables.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
