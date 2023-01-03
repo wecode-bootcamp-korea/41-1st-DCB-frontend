f77c823e13aa228497b8da8926c6c391ed09a15f
sally-dot <doooit135@gmail.com>
2023-01-03 20:27:02 +0900

    Add: 레이아웃 구성 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CiSearch } from 'react-icons/ci';
 import { BsCart3 } from 'react-icons/bs';
 import { HiOutlineBars3 } from 'react-icons/hi2';
@@ -7,21 +8,46 @@ import './Nav.scss';
 const Nav = () => {
   return (
     <div className="nav">
-      <div>
-        <span>회원가입</span>
-        <span>로그인</span>
-        <span>고객센터</span>
+      <div className="navTop">
+        <div className="navFirst">
+          <Link to="/signup" className="navFirstStyle">
+            회원가입
+          </Link>
+          <Link to="/login" className="navFirstStyle">
+            로그인
+          </Link>
+          <Link to="#" className="navFirstStyle">
+            고객센터
+          </Link>
+        </div>
+        <div className="navSecond">
+          <p className="navLogo">SIMPLE</p>
+          <div className="navSearch">
+            <input className="navInput" />
+            <button className="navButton">
+              <CiSearch className="navInputIcon" />
+            </button>
+          </div>
+          <BsCart3 className="navCart" />
+        </div>
       </div>
-      <div>
-        <p className="logo">SIMPLE</p>
-        <div className="divbox">box</div>
-        <CiSearch />
-        <BsCart3 />
-      </div>
-      <div>
-        <HiOutlineBars3 />
-        <span>추천</span>
-        <span>신제품</span>
+      <div className="navThird">
+        <div className="navThirdLeft">
+          <HiOutlineBars3 className="navThirdBar" />
+          <span className="navThirdCat">카테고리</span>
+        </div>
+        <div className="navThirdMid">
+          <Link to="#" className="navThirdList">
+            추천
+          </Link>
+          <Link to="#" className="navThirdList">
+            신제품
+          </Link>
+          <Link to="#" className="navThirdLast">
+            베스트
+          </Link>
+        </div>
+        <div className="navThirdRight" />
       </div>
     </div>
   );
