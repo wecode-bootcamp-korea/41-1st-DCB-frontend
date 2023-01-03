e10b56d9682823c7a3982f8c304f5fcefdd82696
sally-dot <doooit135@gmail.com>
2023-01-03 13:15:28 +0900

    Add: Nav.js, Nav.scss

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div>
-      <h1>nav</h1>
+    <div className="nav">
+      <div>
+        <span>회원가입</span>
+        <span>로그인</span>
+        <span>고객센터</span>
+      </div>
+      <div>
+        <p>SIMPLE</p>
+        <div>box</div>
+        <div>장바구니</div>
+      </div>
+      <div>
+        <div>이미지</div>
+        <span>추천</span>
+        <span>신제품</span>
+      </div>
     </div>
   );
 };
