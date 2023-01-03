2a6da1a2e4cc7d9abcdf3a3a8d823e5e924196e7
Mia <tjwlfif@gmail.com>
2023-01-03 10:29:14 +0900

    Add: main 상단 캐러셀 레이아웃 구현(함수 적용전)

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Products } from './components/Products';
 import './main.scss';
 
 const Main = () => {
   return (
     <div className="main">
-      <div className="eventImages" />
+      <div className="eventImages" style={{ transform: 'translate(0vw)' }}>
+        <div className="imageContainer">
+          <img src="./images/1.jpg" />
+        </div>
+        <div className="imageContainer">
+          <img src="./images/2.jpg" />
+        </div>
+        <div className="imageContainer">
+          <img src="./images/3.jpg" />
+        </div>
+      </div>
       <div className="productsList">
         <Products productName="따끈따끈한 신상" />
         <Products productName="지갑은 가볍게 양손은 무겁게" />
