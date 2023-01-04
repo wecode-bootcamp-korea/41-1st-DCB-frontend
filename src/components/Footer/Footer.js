d247d3cad50497856ddd1e05cf324246a3f0e53c
sally-dot <doooit135@gmail.com>
2023-01-04 16:58:39 +0900

    Add: 멘토님 피드백 반영

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { FaComment } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { LINK_LIST } from './FooterData';
+import { LINK_LIST, INFO_LIST, LINKBTM_LIST } from './FooterData';
 import './Footer.scss';
 
 const Footer = () => {
@@ -10,29 +10,22 @@ const Footer = () => {
     <div className="footer">
       <div className="container">
         <div className="linkBox">
-          <Link className="logo" to="/signup">
-            로그인
-          </Link>
-          <p>고객센터</p>
-          <p>브랜드 스토리</p>
-          <p>블로그</p>
-          <p>인재채용</p>
+          {LINK_LIST.map(list => {
+            return (
+              <Link className="linkBoxLi" to={list.to} key={list}>
+                {list.title}
+              </Link>
+            );
+          })}
         </div>
         <hr className="hr" />
         <div className="footerInfo">
           <div className="infoLeft">
             <p className="fontSize15">(주)DCB컴퍼니 사업자정보</p>
             <div className="fontSize14">
-              <p>서울특별시 강남구 테헤란로 427 대표자: 송은우</p>
-              <p>
-                사업자 등록번호: 530-81-01310, 통신판매업신고번호: 제
-                2022-서울강남-06094
-              </p>
-              <p>
-                개인정보보호책임자: security@wecode, 고객센터: 070-4323-4050
-              </p>
-              <p>E-mail: help@wecode.com</p>
-              <p>제휴문의: help@wecode.com</p>
+              {INFO_LIST.map(info => {
+                return <p key={info}>{info.title}</p>;
+              })}
             </div>
           </div>
           <div className="infoRight">
@@ -51,11 +44,13 @@ const Footer = () => {
         </div>
         <div className="linkBoxBtm">
           <div className="linkBoxBtmLeft">
-            <span className="borderBar">개인정보처리방침</span>
-            <span className="borderBar">이용안내</span>
-            <span className="borderBar">사업자정보확인</span>
-            <span className="borderBar">이용약관</span>
-            <span>이메일무단수집거부</span>
+            {LINKBTM_LIST.map(listbtm => {
+              return (
+                <Link className="borderBar" to={listbtm.to} key={listbtm}>
+                  {listbtm.title}
+                </Link>
+              );
+            })}
           </div>
           <div className="linkBoxBtmRight">
             <span>
@@ -63,13 +58,6 @@ const Footer = () => {
             </span>
           </div>
         </div>
-        {/* {LINK_LIST.map(list => {
-          return (
-            <span className="borderBar" key={list}>
-              {list}
-            </span>
-          );
-        })} */}
       </div>
     </div>
   );
