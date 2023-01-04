03ab05621d29b6dac6711b35c6dcebe7b6d061af
sally-dot <doooit135@gmail.com>
2023-01-04 15:00:49 +0900

    Add: Login 파일들

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import './Login.scss';
 
 const Login = () => {
   return (
-    <div>
-      <h1>hello!</h1>
+    <div className="login">
+      <input
+        className="loginBox"
+        text="text"
+        placeholder="이메일을 입력해주세요"
+      />
+      <input
+        className="loginBox"
+        text="text"
+        placeholder="비밀번호를 입력해주세요"
+      />
+      <div className="checkId">
+        <AiOutlineCheckCircle className="checkBtn" />
+        <span className="checkIdSave">아이디 저장</span>
+      </div>
+      <button className="btnLogin">로그인</button>
+      <button className="btnSignup">회원가입</button>
+      <div className="loginFind">
+        <Link to="#" className="colorWhite margin30">
+          이메일 찾기
+        </Link>
+        <Link to="#" className="colorWhite">
+          비밀번호 찾기
+        </Link>
+      </div>
     </div>
   );
 };
