0afcc664fb0c31d78a26517dcd529b386205931d
sally-dot <doooit135@gmail.com>
2023-01-06 14:04:39 +0900

    Add: login feature 시작

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,32 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 import './Login.scss';
 
 const Login = () => {
+  const [loginInfo, setLoginInfo] = useState({
+    email: '',
+    pw: '',
+  });
+
+  const handleInfo = e => {
+    console.log(e);
+    const { name, value } = e.target;
+    setLoginInfo(prev => ({ ...prev, [name]: value }));
+  };
+
+  const navigate = useNavigate();
+  const gotoSignup = () => navigate('/signup');
+
+  const loginClick = () => {
+    fetch('http://152.67.208.118:3000/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify({ email: loginInfo.email, password: loginInfo.pw }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        localStorage.setItem('Token', result.accessToken);
+      });
+  };
+
   return (
     <div className="login">
       <input
         className="loginBox"
-        text="text"
+        type="text"
         placeholder="이메일을 입력해주세요"
+        value={loginInfo.email}
+        onChange={handleInfo}
       />
       <input
         className="loginBox"
-        text="text"
+        type="password"
         placeholder="비밀번호를 입력해주세요"
+        value={loginInfo.pw}
+        onChange={handleInfo}
       />
       <div className="checkId">
         <AiOutlineCheckCircle className="checkBtn" />
         <span className="checkIdSave">아이디 저장</span>
       </div>
-      <button className="btnLogin">로그인</button>
-      <button className="btnSignup">회원가입</button>
+      <button className="btnLogin" onClick={loginClick}>
+        로그인
+      </button>
+      <button className="btnSignup" onClick={gotoSignup}>
+        회원가입
+      </button>
       <div className="memberFind">
-        <Link to="#" className="colorWhite margin30">
+        <Link to="/" className="colorWhite margin30">
           이메일 찾기
         </Link>
-        <Link to="#" className="colorWhite">
+        <Link to="/" className="colorWhite">
           비밀번호 찾기
         </Link>
       </div>
