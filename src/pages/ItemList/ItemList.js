493da2c1e0d4942019cab828aec543606547ce6f
Mia <tjwlfif@gmail.com>
2023-01-09 16:00:17 +0900

    Add: quary string을 통해 데이터 가져오기 완료

--- src/pages/ItemList/ItemList.js
+++ src/pages/ItemList/ItemList.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Products } from './components/Products';
 import './itemList.scss';
 
 const ItemLists = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [items, setItems] = useState([]);
   const [title, setTitle] = useState('');
 
+  const setOptionParams = e => {
+    searchParams.set('sort', e.target.value);
+    setSearchParams(searchParams);
+  };
+
   useEffect(() => {
-    fetch('http://152.67.208.118:3000/items/?page=1', {
+    fetch(`http://152.67.208.118:3000/items?${searchParams.toString()}`, {
       method: 'GET',
     })
       .then(res => res.json())
@@ -15,7 +22,7 @@ const ItemLists = () => {
         setItems(result.data);
         setTitle(result.data[0].category_name);
       });
-  }, []);
+  }, [searchParams]);
 
   return (
     <div className="itemList">
@@ -30,10 +37,10 @@ const ItemLists = () => {
       <div className="titleArea">{title}</div>
       <div className="sort">
         <p>총 {items.length}개</p>
-        <select className="selectList">
-          <option>최신순</option>
-          <option>가격 낮은 순</option>
-          <option>가격 높은 순</option>
+        <select className="selectList" onChange={setOptionParams}>
+          <option value="new">최신순</option>
+          <option value="cheap">가격 낮은 순</option>
+          <option value="expensive">가격 높은 순</option>
         </select>
       </div>
       <Products items={items} />
