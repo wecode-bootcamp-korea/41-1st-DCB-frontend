85a0b10f89f15d8433eb8b78a3a63819be2def76
sally-dot <doooit135@gmail.com>
2023-01-11 14:46:48 +0900

    Add: 마이페이지 레이아웃 구현

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -5,46 +5,55 @@ import './Mypage.scss';
 const Mypage = () => {
   return (
     <div className="mypage">
-      <div className="grayBack">
-        <div className="boxLeft">
+      <div className="grayWrapper">
+        <div className="grayLeft">
           <p>
             안녕하세요
             <br />
-            오늘도 가심비있는 하루 되세요!
+            오늘도 <span className="textBrown">가심비</span>있는 하루 되세요!
           </p>
-          <button>SIMPLE 회원</button>
+          <button className="buttonStyle">SIMPLE 회원</button>
         </div>
-        <div className="boxRight">
-          <div className="boxRightTop">
-            <div className="boxRightTopFirst">
+        <div className="grayRight">
+          <div className="grayRightTop">
+            <div className="grayRightTopFirst">
               <span>적립금</span>
-              <Link>0원</Link>
+              <Link className="linkStyle" to="#">
+                0원
+              </Link>
             </div>
-            <div className="boxRightTopSecond">
+            <div className="grayRightTopSecond" to="#">
               <span>쿠폰</span>
-              <Link>1개</Link>
+              <Link className="linkStyle">1개</Link>
             </div>
           </div>
-          <div className="boxRightThird">
-            <span>010-1234-5678</span>
-            <Link>정보수정</Link>
+          <div className="grayRightThird">
+            <p>010-1234-5678</p>
+            <Link className="linkStyle" to="#">
+              정보수정
+            </Link>
           </div>
         </div>
       </div>
-      <div className="d">
-        <div className="boxLeft">
-          <p>마이페이지</p>
-          <div className="mypageCat">
-            <li>주문/배송 조회</li>
-            <li>최근 본 상품</li>
-            <li>제휴/대량구매 문의</li>
-            <li>고객센터</li>
+      <div className="whiteWrapper">
+        <div className="whiteInner">
+          <div className="whiteLeft">
+            <p className="title">마이페이지</p>
+            <div className="boxList">
+              <div className="boxLists">주문/배송 조회</div>
+              <div className="boxLists">최근 본 상품</div>
+              <div className="boxLists">제휴/대량구매 문의</div>
+              <div className="boxLists">고객센터</div>
+            </div>
           </div>
-        </div>
-        <div className="boxRight">
-          <div>
-            <span>주문배송현황</span>
-            <button>취소교환반품 내역</button>
+          <div className="whiteRight">
+            <div className="orderStatus">
+              <p>
+                주문배송현황<span>(최근 3개월 기준)</span>
+              </p>
+              <button>취소교환반품 내역</button>
+            </div>
+            <div className="blankPage" />
           </div>
         </div>
       </div>
