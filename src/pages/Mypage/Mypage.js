56456e35a266ad1deb06e02c7ba5fc0aafabffef
sally-dot <doooit135@gmail.com>
2023-01-11 11:20:13 +0900

    Add: 내용추가

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -15,21 +15,39 @@ const Mypage = () => {
           <button>SIMPLE 회원</button>
         </div>
         <div className="boxRight">
-          <div className="">
-            <span>적립금</span>
-            <Link>0원</Link>
+          <div className="boxRightTop">
+            <div className="boxRightTopFirst">
+              <span>적립금</span>
+              <Link>0원</Link>
+            </div>
+            <div className="boxRightTopSecond">
+              <span>쿠폰</span>
+              <Link>1개</Link>
+            </div>
           </div>
-          <div>
-            <span>쿠폰</span>
-            <Link>1개</Link>
-          </div>
-          <div>
+          <div className="boxRightThird">
             <span>010-1234-5678</span>
             <Link>정보수정</Link>
           </div>
         </div>
       </div>
-      <div />
+      <div className="d">
+        <div className="boxLeft">
+          <p>마이페이지</p>
+          <div className="mypageCat">
+            <li>주문/배송 조회</li>
+            <li>최근 본 상품</li>
+            <li>제휴/대량구매 문의</li>
+            <li>고객센터</li>
+          </div>
+        </div>
+        <div className="boxRight">
+          <div>
+            <span>주문배송현황</span>
+            <button>취소교환반품 내역</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
