22ea938908f8f8911e19a22f224ce499ef41429d
Mia <tjwlfif@gmail.com>
2023-01-11 13:06:30 +0900

    Add: scroll btn 구현 완료

--- src/components/ScrollBtn/ScrollBtn.js
+++ src/components/ScrollBtn/ScrollBtn.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsArrowUpCircle } from 'react-icons/bs';
 import './scrollBtn.scss';
 
 const ScrollBtn = () => {
+  const [showBtn, setShowBtn] = useState(false);
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    const handleShowButton = () => {
+      if (window.scrollY > 200) {
+        setShowBtn(true);
+      } else {
+        setShowBtn(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleShowButton);
+    return () => {
+      window.removeEventListener('scroll', handleShowButton);
+    };
+  }, []);
+
   return (
-    <div className="scrollBtn">
-      <BsArrowUpCircle size="50px" className="btnIcon" />
+    <div className="scrollBtn" onClick={scrollToTop}>
+      <BsArrowUpCircle size="50px" className={showBtn ? 'btnIcon' : 'hidden'} />
     </div>
   );
 };
